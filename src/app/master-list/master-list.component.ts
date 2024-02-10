4ddb346fee6062d99165374c79b45dabb88f76c4
Freejinn <141774931+Freejinn@users.noreply.github.com>
2024-02-10 09:12:16 -0700

    login link on master-list page

--- src/app/master-list/master-list.component.ts
+++ src/app/master-list/master-list.component.ts
@@ -1,8 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { PathfinderListService } from './pathfinder-list.service';
 import { Observable, tap } from 'rxjs';
 
 import { ScoutGroup, SkillParent } from '../models/skill.model';
+import { AppState } from '../reducers';
+import { Store, select } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { isLoggedIn, isLoggedOut } from '../auth/auth.selectors';
 
 @Component({
   selector: 'app-master-list',
@@ -10,13 +14,30 @@ import { ScoutGroup, SkillParent } from '../models/skill.model';
   styleUrls: ['./master-list.component.css'],
   providers: [PathfinderListService]
 })
-export class MasterListComponent {
+export class MasterListComponent implements OnInit {
 
-  listService = inject(PathfinderListService);
+  isLoggedIn$:Observable<boolean>;
+  isLoggedOut$:Observable<boolean>;
+
+  constructor(
+    private store: Store<AppState>,
+    private route: Router
+    ) {}
+
+    listService = inject(PathfinderListService);
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.store
+    .pipe(select(isLoggedIn));
+
+    this.isLoggedOut$ = this.store
+    .pipe(select(isLoggedOut));
+  }
 
   skillsList$: Observable<ScoutGroup[]> = this.listService
   .getSkillsList()
   .pipe(
     tap((res)=> console.log('res', res))
   );
+
 }
